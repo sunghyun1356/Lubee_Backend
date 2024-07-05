e6ea448fc7d55e51d636b2f3105b6270053d3703
parking0 <p980710@gmail.com>
2024-07-05 17:29:03 +0900

    [refactor] : modify Calendar and DateComment entities

--- src/main/java/com/Lubee/Lubee/date_comment/domain/DateComment.java
+++ src/main/java/com/Lubee/Lubee/date_comment/domain/DateComment.java
@@ -1,15 +1,14 @@
 package com.Lubee.Lubee.date_comment.domain;
 
+import com.Lubee.Lubee.calendar.domain.Calendar;
 import com.Lubee.Lubee.common.BaseEntity;
 import com.Lubee.Lubee.couple.domain.Couple;
-import com.Lubee.Lubee.memory.domain.Memory;
 import com.Lubee.Lubee.user.domain.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 @Entity
 @Getter
@@ -29,12 +28,18 @@ public class DateComment extends BaseEntity {
     @JoinColumn(name = "couple_id", nullable = false)
     private Couple couple;
 
-    @ManyToOne
-    @JoinColumn(name = "memory_id", nullable = false)
-    private Memory memory;
-
-    @Temporal(TemporalType.DATE)
-    private Date date;
-
     private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "calendar_id", nullable = false)
+    private Calendar calendar;      //calendar, datecomment는 양방향 일대다
+
+    @Builder
+    public DateComment(User user, Couple couple, String content, Calendar calendar){
+        this.user = user;
+        this.couple = couple;
+        this.content = content;
+        this.calendar = calendar;
+    }
+
 }
