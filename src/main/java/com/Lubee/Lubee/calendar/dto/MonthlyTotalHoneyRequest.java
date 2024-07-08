fd239d9d23f878fff6bf9fca56100b42e1c460da
parking0 <p980710@gmail.com>
2024-07-08 18:02:29 +0900

    [feat] implement Honey

---
+++ src/main/java/com/Lubee/Lubee/calendar/dto/MonthlyTotalHoneyRequest.java
@@ -0,0 +1,12 @@
+package com.Lubee.Lubee.calendar.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MonthlyTotalHoneyRequest {
+
+    private int year;
+    private int month;
+}
