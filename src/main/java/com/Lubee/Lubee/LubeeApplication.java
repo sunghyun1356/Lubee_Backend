bde3d3608d094c1879c4ebd8d2089e36cde97ef7
unknown <sunghyun1356@naver.com>
2024-07-22 15:43:50 +0900

    [feat] swagger url add

--- src/main/java/com/Lubee/Lubee/LubeeApplication.java
+++ src/main/java/com/Lubee/Lubee/LubeeApplication.java
@@ -1,11 +1,14 @@
 package com.Lubee.Lubee;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "https://lubee.site", description = "Default Server URL")})
 public class LubeeApplication {
 
 	static {System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");}
