17874090e3f619ddd70e4dded46d98d9119e6ced
parking0 <p980710@gmail.com>
2024-07-23 14:15:49 +0900

    [refactor] modify LocationDto

--- src/main/java/com/Lubee/Lubee/location/dto/LocationDto.java
+++ src/main/java/com/Lubee/Lubee/location/dto/LocationDto.java
@@ -11,17 +11,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LocationDto {
 
+    private Long locationId;
     private String name;
     private String parcelBaseAddress;
 
     @Builder
-    public LocationDto(String name, String parcelBaseAddress) {
+    public LocationDto(Long locationId, String name, String parcelBaseAddress) {
+        this.locationId = locationId;
         this.name = name;
         this.parcelBaseAddress = parcelBaseAddress;
     }
 
     public static LocationDto from(Location location) {
         return new LocationDto(
+                location.getLocation_id(),
                 location.getName(),
                 location.getParcelBaseAddress()
         );
